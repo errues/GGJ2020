9059b1faf8e4201903838bb63c04ba2967eadefa
errues <errues@gmail.com>
2020-01-31 21:35:55 +0100

    Movimiento básico

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants {
+    public static float GRAVITY = -.1f;
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float movementSpeed;
+    
+    private CharacterController characterController;
+
+    private float fallingSpeed;
+
+    private void Awake() {
+        characterController = GetComponent<CharacterController>();
+    }
+    private void Update() {
+        fallingSpeed += Constants.GRAVITY;
+        characterController.Move(new Vector3(Input.GetAxis("Horizontal"), fallingSpeed, 0) * Time.deltaTime * movementSpeed);
+
+        if (characterController.isGrounded) {
+            fallingSpeed = 0;
+        }
+    }
+}
