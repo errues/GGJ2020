7ed52e07cc4135fd304989c849e9d70604ffa21f
errues <errues@gmail.com>
2020-02-01 19:20:43 +0100

    Música

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour {
+    public AudioClip musicClip;
+
+    private AudioSource audioSource;
+
+    private void Awake() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Start() {
+        StartMusic();
+    }
+
+    public void StartMusic() {
+        audioSource.clip = musicClip;
+        audioSource.Play();
+    }
+
+    public void StopMusic() {
+        audioSource.Stop();
+    }
+}
