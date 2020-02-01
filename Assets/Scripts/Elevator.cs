119cc9f444442296150ecfa5b2980f4387cb6eb2
errues <errues@gmail.com>
2020-02-01 04:55:24 +0100

    Ascensor funcionando en modelo

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -3,18 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Elevator : Interactable {
+    [Header("References")]
     public GameObject secondFloorInteractionIndicator;
+    public Transform downLeftDoor;
+    public Transform downRightDoor;
+    public Transform topLeftDoor;
+    public Transform topRightDoor;
 
+    [Header("Times")]
     public float positioningTime;
     public float enteringTime;
     public float travelingTime;
 
+    [Header("Distances")]
     public float backDistance;
 
     private Vector3 downFrontPosition;
     private Vector3 downBackPosition;
     private Vector3 topFrontPosition;
     private Vector3 topBackPosition;
+
+    private bool doorsMoving;
+    private bool topDoors;
+    private bool openingDoors;
+
+    private float alpha;
+    private float initialTime;
 
     private void Awake() {
         downFrontPosition = transform.position;
@@ -23,22 +37,48 @@ public class Elevator : Interactable {
         topBackPosition = new Vector3(transform.position.x, Bunker.instance.secondLevelHeight, transform.position.z + backDistance);
     }
 
+    protected override void Update() {
+        base.Update();
+
+        if (doorsMoving) {
+            alpha = Mathf.Clamp((Time.time - initialTime) / positioningTime, 0, 1);
+
+            Vector3 scale = new Vector3(openingDoors ? 1 - alpha : alpha, 1, 1);
+
+            if (topDoors) {
+                topLeftDoor.localScale = scale;
+                topRightDoor.localScale = scale;
+            } else {
+                downLeftDoor.localScale = scale;
+                downRightDoor.localScale = scale;
+            }
+
+            if ((alpha == 0 && !openingDoors) || (alpha == 1 && openingDoors)) {
+                doorsMoving = false;
+            }
+        }
+    }
+
     protected override void InteractLoop() {
         if (Input.GetAxisRaw("Use") != 0 && !player.AutoMoving) {
             if (player.OnFirstFloor) {
                 player.AddAutoMovement(downFrontPosition, positioningTime);
                 player.AddAutoMovement(downBackPosition, enteringTime);
+                player.AddAutoMovement(downBackPosition, positioningTime);
                 player.AddAutoMovement(topBackPosition, travelingTime);
                 player.AddAutoMovement(topFrontPosition, enteringTime);
-                player.OnFirstFloor = false;
+                player.AddAutoMovement(topFrontPosition, positioningTime);
             } else {
                 player.AddAutoMovement(topFrontPosition, positioningTime);
                 player.AddAutoMovement(topBackPosition, enteringTime);
+                player.AddAutoMovement(topBackPosition, positioningTime);
                 player.AddAutoMovement(downBackPosition, travelingTime);
                 player.AddAutoMovement(downFrontPosition, enteringTime);
-                player.OnFirstFloor = true;
+                player.AddAutoMovement(downFrontPosition, positioningTime);
             }
 
+            StartCoroutine(DoorsSequence(player.OnFirstFloor));
+            player.OnFirstFloor = !player.OnFirstFloor;
             player.StartAutomovement();
             HideInteraction();
         }
@@ -56,5 +96,69 @@ public class Elevator : Interactable {
     protected override void HideInteraction() {
         base.HideInteraction();
         secondFloorInteractionIndicator.SetActive(false);
+    }
+
+    private void OpenDownDoors() {
+        doorsMoving = true;
+        topDoors = false;
+        openingDoors = true;
+
+        initialTime = Time.time;
+    }
+
+    private void CloseDownDoors() {
+        doorsMoving = true;
+        topDoors = false;
+        openingDoors = false;
+
+        initialTime = Time.time;
+    }
+
+    private void OpenTopDoors() {
+        doorsMoving = true;
+        topDoors = true;
+        openingDoors = true;
+
+        initialTime = Time.time;
+    }
+
+    private void CloseTopDoors() {
+        doorsMoving = true;
+        topDoors = true;
+        openingDoors = false;
+
+        initialTime = Time.time;
+    }
+
+    private IEnumerator DoorsSequence(bool goingUp) {
+        if (goingUp) {
+            OpenDownDoors();
+        } else {
+            OpenTopDoors();
+        }
+
+        yield return new WaitForSeconds(positioningTime + enteringTime);
+
+        if (goingUp) {
+            CloseDownDoors();
+        } else {
+            CloseTopDoors();
+        }
+
+        yield return new WaitForSeconds(positioningTime + travelingTime);
+
+        if (goingUp) {
+            OpenTopDoors();
+        } else {
+            OpenDownDoors();
+        }
+
+        yield return new WaitForSeconds(positioningTime);
+
+        if (goingUp) {
+            CloseTopDoors();
+        } else {
+            CloseDownDoors();
+        }
     }
 }
