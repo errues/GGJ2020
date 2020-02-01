9493f662aba42f41c0be981b43f2947ec9e67238
errues <errues@gmail.com>
2020-02-01 03:55:11 +0100

    Ajuste de cámara

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform target;
+    public float speed;
+
+    private Vector3 offset;
+
+    private void Awake() {
+        offset = transform.position - target.position;
+    }
+
+    private void Update() {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x + offset.x, target.position.y + offset.y, transform.position.z), Time.deltaTime * speed);
+    }
+}
