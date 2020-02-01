3f6ed07001bc875d60cf55af534b27f13822ba45
errues <errues@gmail.com>
2020-02-01 03:18:52 +0100

    Funcionamiento básico de ascensor

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -2,6 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Elevator : MonoBehaviour {
+public class Elevator : Interactable {
+    public GameObject secondFloorInteractionIndicator;
 
+    public float positioningTime;
+    public float enteringTime;
+    public float travelingTime;
+
+    public float backDistance;
+
+    private Vector3 downFrontPosition;
+    private Vector3 downBackPosition;
+    private Vector3 topFrontPosition;
+    private Vector3 topBackPosition;
+
+    private void Awake() {
+        downFrontPosition = transform.position;
+        downBackPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + backDistance);
+        topFrontPosition = new Vector3(transform.position.x, Bunker.instance.secondLevelHeight, transform.position.z);
+        topBackPosition = new Vector3(transform.position.x, Bunker.instance.secondLevelHeight, transform.position.z + backDistance);
+    }
+
+    protected override void InteractLoop() {
+        if (Input.GetAxisRaw("Use") != 0 && !player.AutoMoving) {
+            if (player.OnFirstFloor) {
+                player.AddAutoMovement(downFrontPosition, positioningTime);
+                player.AddAutoMovement(downBackPosition, enteringTime);
+                player.AddAutoMovement(topBackPosition, travelingTime);
+                player.AddAutoMovement(topFrontPosition, enteringTime);
+                player.OnFirstFloor = false;
+            } else {
+                player.AddAutoMovement(topFrontPosition, positioningTime);
+                player.AddAutoMovement(topBackPosition, enteringTime);
+                player.AddAutoMovement(downBackPosition, travelingTime);
+                player.AddAutoMovement(downFrontPosition, enteringTime);
+                player.OnFirstFloor = true;
+            }
+
+            player.StartAutomovement();
+            HideInteraction();
+        }
+    }
+
+    protected override void ShowInteraction() {
+        if (player.OnFirstFloor) {
+            interactionIndicator.SetActive(true);
+        } else {
+            secondFloorInteractionIndicator.SetActive(true);
+        }
+        ShowingInteraction = true;
+    }
+
+    protected override void HideInteraction() {
+        base.HideInteraction();
+        secondFloorInteractionIndicator.SetActive(false);
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    private class AutoMovement {
+        public Vector3 targetPosition;
+        public float time;
+
+        public AutoMovement(Vector3 p, float t) {
+            targetPosition = p;
+            time = t;
+        }
+    }
+
     public float movementSpeed;
     public float inLadderMovementSpeed;
 
     public float frontZPos = 0;
     public float backZPos = 1;
 
+    private Queue<AutoMovement> autoMovements;
+    private Vector3 initialPosition;
+    private Vector3 targetPosition;
+    private float initialTime;
+    private float targetTime;
+    private float autoMoveAlpha;
+
     public bool OnFirstFloor { get; set; }
     public bool InLadder { get; set; }
+    public bool AutoMoving { get; set; }
     
     private CharacterController characterController;
 
@@ -18,19 +36,35 @@ public class Player : MonoBehaviour {
 
     private void Awake() {
         characterController = GetComponent<CharacterController>();
-        OnFirstFloor = true;
+        OnFirstFloor = transform.position.y < Bunker.instance.secondLevelHeight - 1;
         InLadder = false;
+
+        autoMovements = new Queue<AutoMovement>();
     }
 
     private void Update() {
-        if (InLadder) {
-            characterController.Move(new Vector3(0, Input.GetAxisRaw("Vertical"), 0) * Time.deltaTime * inLadderMovementSpeed);
+        if (AutoMoving) {
+            autoMoveAlpha = Mathf.Clamp((Time.time - initialTime) / targetTime, 0, 1);
+            transform.position = Vector3.Lerp(initialPosition, targetPosition, AnimationCurve.EaseInOut(0, 0, 1, 1).Evaluate(autoMoveAlpha));
+
+            if (autoMoveAlpha == 1) {                
+                if (autoMovements.Count > 0) {
+                    NextAutoMovement();
+                } else {
+                    AutoMoving = false;
+                    characterController.enabled = true;
+                }
+            }
         } else {
-            fallingSpeed += Constants.GRAVITY;
-            characterController.Move(new Vector3(Input.GetAxis("Horizontal"), fallingSpeed, 0) * Time.deltaTime * movementSpeed);
+            if (InLadder) {
+                characterController.Move(new Vector3(0, Input.GetAxisRaw("Vertical"), 0) * Time.deltaTime * inLadderMovementSpeed);
+            } else {
+                fallingSpeed += Constants.GRAVITY;
+                characterController.Move(new Vector3(Input.GetAxis("Horizontal"), fallingSpeed, 0) * Time.deltaTime * movementSpeed);
         
-            if (characterController.isGrounded) {
-                fallingSpeed = Constants.GRAVITY;
+                if (characterController.isGrounded) {
+                    fallingSpeed = Constants.GRAVITY;
+                }
             }
         }
     }
@@ -71,5 +105,27 @@ public class Player : MonoBehaviour {
         characterController.enabled = false;
         transform.position = pos;
         characterController.enabled = true;
+    }
+
+    public void AddAutoMovement(Vector3 targetPosition, float time) {
+        autoMovements.Enqueue(new AutoMovement(targetPosition, time));
+    }
+
+    public void StartAutomovement() {
+        if (autoMovements.Count > 0) {
+            characterController.enabled = false;
+            NextAutoMovement();
+        }
+    }
+
+    public void NextAutoMovement() {
+        AutoMovement am = autoMovements.Dequeue();
+        initialPosition = transform.position;
+        targetPosition = am.targetPosition;
+        initialTime = Time.time;
+        targetTime = am.time;
+
+        autoMoveAlpha = 0;
+        AutoMoving = true;
     }
 }
