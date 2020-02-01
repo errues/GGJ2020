94e2a064ebcfd62bf70227101f43565e9f2740e1
Alejandro Guerrero Martínez <alejandroguerrero.gr@gmail.com>
2020-02-01 15:01:50 +0100

    Orgainzación Critical Interactables y estados

---
+++ Assets/Scripts/Critical Interactables/BunkerDoor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunkerDoor : CriticalInteractable {
+    [Header("Bunker Door")]
+    public float pushButtonFix = 0.1f;
+
+    private bool finishedRepair;
+
+    protected override void InteractLoop() {
+        if (Input.GetButtonDown("Use") && (repairState != RepairState.GOOD || !finishedRepair)) {
+            criticalState = Mathf.Clamp01(criticalState + pushButtonFix);
+            finishedRepair = criticalState == 1;
+
+            if (finishedRepair) {
+                HideInteraction();
+            }
+        }
+
+        if (finishedRepair && repairState == RepairState.NEEDREPAIR) {
+            ShowInteraction();
+        }
+    }
+}
--- Assets/Scripts/Critical Interactables/CriticalInteractable.cs
+++ Assets/Scripts/Critical Interactables/CriticalInteractable.cs
@@ -3,9 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CriticalInteractable : Interactable {
+    public enum RepairState{
+        GOOD,
+        NEEDREPAIR,
+        CRITICAL
+    }
+
+    [Range(0, 1)]
     public float criticalSpeed = 0.1f;
 
+    [Header("Repair States")]
+    [Range(0, 1)]
+    public float noNeedRepairState = 0.8f;
+    [Range(0, 1)]
+    public float criticalRepairState = 0.33f;
+
     protected float criticalState;
+    protected RepairState repairState;
 
     protected void Awake() {
         RestoreCriticalState();
@@ -22,11 +36,20 @@ public class CriticalInteractable : Interactable {
     }
 
     public virtual void ApplyCriticalState() {
-        GetComponentInChildren<MeshRenderer>().material.color = Color.Lerp(Color.red, Color.white, criticalState);
+        if (criticalState < criticalRepairState) {
+            repairState = RepairState.CRITICAL;
+        } else if (criticalState < noNeedRepairState) {
+            repairState = RepairState.NEEDREPAIR;
+        } else {
+            repairState = RepairState.GOOD;
+        }
+
+        GetComponentInChildren<Renderer>().material.color = Color.Lerp(Color.red, Color.white, criticalState);
     }
 
     public void RestoreCriticalState() {
         criticalState = 1f;
+        repairState = RepairState.GOOD;
     }
 
     private void CriticalFinish() {
